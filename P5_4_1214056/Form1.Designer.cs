c33fd0e91589de49cb58561230190832e765c3b1
Rachma <rachmanurhalizastudent@gmail.com>
2022-10-24 11:50:40 +0700

    Add project files.

---
+++ P5_4_1214056/Form1.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace P5_4_1214056
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbKomposer = new System.Windows.Forms.CheckBox();
+            this.cbVokal = new System.Windows.Forms.CheckBox();
+            this.cbDrum = new System.Windows.Forms.CheckBox();
+            this.cbPiano = new System.Windows.Forms.CheckBox();
+            this.cbKonduktor = new System.Windows.Forms.CheckBox();
+            this.cbSaxophone = new System.Windows.Forms.CheckBox();
+            this.cbGitar = new System.Windows.Forms.CheckBox();
+            this.cbBiola = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.jadwal4 = new System.Windows.Forms.RadioButton();
+            this.jadwal3 = new System.Windows.Forms.RadioButton();
+            this.jadwal2 = new System.Windows.Forms.RadioButton();
+            this.jadwal1 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightPink;
+            this.label1.Font = new System.Drawing.Font("Elephant", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MediumBlue;
+            this.label1.Location = new System.Drawing.Point(275, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FORM PENDAFTARAN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Imprint MT Shadow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(275, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Imprint MT Shadow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(228, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Jenis Kelamin";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Imprint MT Shadow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(226, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tanggal Lahir";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(379, 79);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(200, 20);
+            this.textBox.TabIndex = 4;
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "Perempuan",
+            "Laki-Laki"});
+            this.comboBox.Location = new System.Drawing.Point(379, 114);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(121, 21);
+            this.comboBox.TabIndex = 5;
+            this.comboBox.Text = "--Pilih Jenis Kelamin--";
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(379, 152);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker.TabIndex = 6;
+            this.dateTimePicker.Value = new System.DateTime(2022, 10, 16, 6, 46, 0, 0);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbKomposer);
+            this.groupBox1.Controls.Add(this.cbVokal);
+            this.groupBox1.Controls.Add(this.cbDrum);
+            this.groupBox1.Controls.Add(this.cbPiano);
+            this.groupBox1.Controls.Add(this.cbKonduktor);
+            this.groupBox1.Controls.Add(this.cbSaxophone);
+            this.groupBox1.Controls.Add(this.cbGitar);
+            this.groupBox1.Controls.Add(this.cbBiola);
+            this.groupBox1.Location = new System.Drawing.Point(170, 193);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 125);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pilihan Kelas";
+            // 
+            // cbKomposer
+            // 
+            this.cbKomposer.AutoSize = true;
+            this.cbKomposer.ForeColor = System.Drawing.Color.Black;
+            this.cbKomposer.Location = new System.Drawing.Point(108, 95);
+            this.cbKomposer.Name = "cbKomposer";
+            this.cbKomposer.Size = new System.Drawing.Size(73, 17);
+            this.cbKomposer.TabIndex = 7;
+            this.cbKomposer.Text = "Komposer";
+            this.cbKomposer.UseVisualStyleBackColor = true;
+            // 
+            // cbVokal
+            // 
+            this.cbVokal.AutoSize = true;
+            this.cbVokal.ForeColor = System.Drawing.Color.Black;
+            this.cbVokal.Location = new System.Drawing.Point(108, 70);
+            this.cbVokal.Name = "cbVokal";
+            this.cbVokal.Size = new System.Drawing.Size(53, 17);
+            this.cbVokal.TabIndex = 6;
+            this.cbVokal.Text = "Vocal";
+            this.cbVokal.UseVisualStyleBackColor = true;
+            // 
+            // cbDrum
+            // 
+            this.cbDrum.AutoSize = true;
+            this.cbDrum.ForeColor = System.Drawing.Color.Black;
+            this.cbDrum.Location = new System.Drawing.Point(108, 45);
+            this.cbDrum.Name = "cbDrum";
+            this.cbDrum.Size = new System.Drawing.Size(51, 17);
+            this.cbDrum.TabIndex = 5;
+            this.cbDrum.Text = "Drum";
+            this.cbDrum.UseVisualStyleBackColor = true;
+            this.cbDrum.CheckedChanged += new System.EventHandler(this.cbDrum_CheckedChanged);
+            // 
+            // cbPiano
+            // 
+            this.cbPiano.AutoSize = true;
+            this.cbPiano.ForeColor = System.Drawing.Color.Black;
+            this.cbPiano.Location = new System.Drawing.Point(108, 20);
+            this.cbPiano.Name = "cbPiano";
+            this.cbPiano.Size = new System.Drawing.Size(53, 17);
+            this.cbPiano.TabIndex = 4;
+            this.cbPiano.Text = "Piano";
+            this.cbPiano.UseVisualStyleBackColor = true;
+            // 
+            // cbKonduktor
+            // 
+            this.cbKonduktor.AutoSize = true;
+            this.cbKonduktor.ForeColor = System.Drawing.Color.Black;
+            this.cbKonduktor.Location = new System.Drawing.Point(7, 95);
+            this.cbKonduktor.Name = "cbKonduktor";
+            this.cbKonduktor.Size = new System.Drawing.Size(75, 17);
+            this.cbKonduktor.TabIndex = 3;
+            this.cbKonduktor.Text = "Konduktor";
+            this.cbKonduktor.UseVisualStyleBackColor = true;
+            // 
+            // cbSaxophone
+            // 
+            this.cbSaxophone.AutoSize = true;
+            this.cbSaxophone.ForeColor = System.Drawing.Color.Black;
+            this.cbSaxophone.Location = new System.Drawing.Point(7, 70);
+            this.cbSaxophone.Name = "cbSaxophone";
+            this.cbSaxophone.Size = new System.Drawing.Size(80, 17);
+            this.cbSaxophone.TabIndex = 2;
+            this.cbSaxophone.Text = "Saxophone";
+            this.cbSaxophone.UseVisualStyleBackColor = true;
+            // 
+            // cbGitar
+            // 
+            this.cbGitar.AutoSize = true;
+            this.cbGitar.ForeColor = System.Drawing.Color.Black;
+            this.cbGitar.Location = new System.Drawing.Point(7, 45);
+            this.cbGitar.Name = "cbGitar";
+            this.cbGitar.Size = new System.Drawing.Size(48, 17);
+            this.cbGitar.TabIndex = 1;
+            this.cbGitar.Text = "Gitar";
+            this.cbGitar.UseVisualStyleBackColor = true;
+            // 
+            // cbBiola
+            // 
+            this.cbBiola.AutoSize = true;
+            this.cbBiola.ForeColor = System.Drawing.Color.Black;
+            this.cbBiola.Location = new System.Drawing.Point(7, 20);
+            this.cbBiola.Name = "cbBiola";
+            this.cbBiola.Size = new System.Drawing.Size(49, 17);
+            this.cbBiola.TabIndex = 0;
+            this.cbBiola.Text = "Biola";
+            this.cbBiola.UseVisualStyleBackColor = true;
+            this.cbBiola.CheckedChanged += new System.EventHandler(this.cbBiola_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.jadwal4);
+            this.groupBox2.Controls.Add(this.jadwal3);
+            this.groupBox2.Controls.Add(this.jadwal2);
+            this.groupBox2.Controls.Add(this.jadwal1);
+            this.groupBox2.Location = new System.Drawing.Point(445, 193);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 125);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Pilihan Jadwal";
+            // 
+            // jadwal4
+            // 
+            this.jadwal4.AutoSize = true;
+            this.jadwal4.ForeColor = System.Drawing.Color.Black;
+            this.jadwal4.Location = new System.Drawing.Point(18, 91);
+            this.jadwal4.Name = "jadwal4";
+            this.jadwal4.Size = new System.Drawing.Size(120, 17);
+            this.jadwal4.TabIndex = 3;
+            this.jadwal4.TabStop = true;
+            this.jadwal4.Text = "Minggu,13.00-17.00";
+            this.jadwal4.UseVisualStyleBackColor = true;
+            this.jadwal4.CheckedChanged += new System.EventHandler(this.jadwal4_CheckedChanged);
+            // 
+            // jadwal3
+            // 
+            this.jadwal3.AutoSize = true;
+            this.jadwal3.ForeColor = System.Drawing.Color.Black;
+            this.jadwal3.Location = new System.Drawing.Point(18, 67);
+            this.jadwal3.Name = "jadwal3";
+            this.jadwal3.Size = new System.Drawing.Size(175, 17);
+            this.jadwal3.TabIndex = 2;
+            this.jadwal3.TabStop = true;
+            this.jadwal3.Text = "Sabtu dan Minggu, 09.00-11.00";
+            this.jadwal3.UseVisualStyleBackColor = true;
+            this.jadwal3.CheckedChanged += new System.EventHandler(this.jadwal3_CheckedChanged);
+            // 
+            // jadwal2
+            // 
+            this.jadwal2.AutoSize = true;
+            this.jadwal2.ForeColor = System.Drawing.Color.Black;
+            this.jadwal2.Location = new System.Drawing.Point(18, 43);
+            this.jadwal2.Name = "jadwal2";
+            this.jadwal2.Size = new System.Drawing.Size(169, 17);
+            this.jadwal2.TabIndex = 1;
+            this.jadwal2.TabStop = true;
+            this.jadwal2.Text = "Selasa dan Kamis,14.00-16.00";
+            this.jadwal2.UseVisualStyleBackColor = true;
+            this.jadwal2.CheckedChanged += new System.EventHandler(this.jadwal2_CheckedChanged);
+            // 
+            // jadwal1
+            // 
+            this.jadwal1.AutoSize = true;
+            this.jadwal1.ForeColor = System.Drawing.Color.Black;
+            this.jadwal1.Location = new System.Drawing.Point(18, 19);
+            this.jadwal1.Name = "jadwal1";
+            this.jadwal1.Size = new System.Drawing.Size(162, 17);
+            this.jadwal1.TabIndex = 0;
+            this.jadwal1.TabStop = true;
+            this.jadwal1.Text = "Senin dan Rabu,14.00-16.00";
+            this.jadwal1.UseVisualStyleBackColor = true;
+            this.jadwal1.CheckedChanged += new System.EventHandler(this.jadwal1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(275, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Tampilkan";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(409, 361);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Selesai";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightPink;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.DimGray;
+            this.Name = "Form1";
+            this.Text = "ASTRA MUSIC SCHOOL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox cbKomposer;
+        private System.Windows.Forms.CheckBox cbVokal;
+        private System.Windows.Forms.CheckBox cbDrum;
+        private System.Windows.Forms.CheckBox cbPiano;
+        private System.Windows.Forms.CheckBox cbKonduktor;
+        private System.Windows.Forms.CheckBox cbSaxophone;
+        private System.Windows.Forms.CheckBox cbGitar;
+        private System.Windows.Forms.CheckBox cbBiola;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton jadwal4;
+        private System.Windows.Forms.RadioButton jadwal3;
+        private System.Windows.Forms.RadioButton jadwal2;
+        private System.Windows.Forms.RadioButton jadwal1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ P5_4_1214056/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P5_4_1214056
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        string kelas = "";
+        string jadwal = "";
+        private void jadwal1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (jadwal1.Checked)
+            {
+                jadwal = "Senin dan Rabu, 14.00 - 16.00";
+            }
+            else if (jadwal2.Checked)
+            {
+                jadwal = "Selasa dan Kamis, 14.00 - 16.00";
+            }
+            else if (jadwal3.Checked)
+            {
+                jadwal = "Sabtu dan Minggu, 09.00 - 11.00";
+            }
+            else if (jadwal4.Checked)
+            {
+                jadwal = "Minggu, 13.00 - 17.00";
+            }
+        }
+
+        private void jadwal2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (jadwal1.Checked)
+            {
+                jadwal = "Senin dan Rabu, 14.00 - 16.00";
+            }
+            else if (jadwal2.Checked)
+            {
+                jadwal = "Selasa dan Kamis, 14.00 - 16.00";
+            }
+            else if (jadwal3.Checked)
+            {
+                jadwal = "Sabtu dan Minggu, 09.00 - 11.00";
+            }
+            else if (jadwal4.Checked)
+            {
+                jadwal = "Minggu, 13.00 - 17.00";
+            }
+        }
+
+        private void jadwal3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (jadwal1.Checked)
+            {
+                jadwal = "Senin dan Rabu, 14.00 - 16.00";
+            }
+            else if (jadwal2.Checked)
+            {
+                jadwal = "Selasa dan Kamis, 14.00 - 16.00";
+            }
+            else if (jadwal3.Checked)
+            {
+                jadwal = "Sabtu dan Minggu, 09.00 - 11.00";
+            }
+            else if (jadwal4.Checked)
+            {
+                jadwal = "Minggu, 13.00 - 17.00";
+            }
+        }
+
+        private void jadwal4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (jadwal1.Checked)
+            {
+                jadwal = "Senin dan Rabu, 14.00 - 16.00";
+            }
+            else if (jadwal2.Checked)
+            {
+                jadwal = "Selasa dan Kamis, 14.00 - 16.00";
+            }
+            else if (jadwal3.Checked)
+            {
+                jadwal = "Sabtu dan Minggu, 09.00 - 11.00";
+            }
+            else if (jadwal4.Checked)
+            {
+                jadwal = "Minggu, 13.00 - 17.00";
+            }
+        }
+
+        private void cbBiola_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbBiola.Checked)
+            {
+                kelas = kelas + "Biola";
+            }
+            if (cbDrum.Checked)
+            {
+                kelas = kelas + "Drum";
+            }
+            if (cbGitar.Checked)
+            {
+                kelas = kelas + "Gitar";
+            }
+            if (cbKomposer.Checked)
+            {
+                kelas = kelas + "Komposer";
+            }
+            if (cbKonduktor.Checked)
+            {
+                kelas = kelas + "Konduktor";
+            }
+            if (cbPiano.Checked)
+            {
+                kelas = kelas + "Piano";
+            }
+            if (cbSaxophone.Checked)
+            {
+                kelas = kelas + "Saxophone";
+            }
+            if (cbVokal.Checked)
+            {
+                kelas = kelas + "Vokal";
+            }
+            if (kelas == "")
+            {
+                MessageBox.Show("Harus memilih salah satu dari pilihan kelas", "Warning!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void cbDrum_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbBiola.Checked)
+            {
+                kelas = kelas + "Biola";
+            }
+            if (cbDrum.Checked)
+            {
+                kelas = kelas + "Drum";
+            }
+            if (cbGitar.Checked)
+            {
+                kelas = kelas + "Gitar";
+            }
+            if (cbKomposer.Checked)
+            {
+                kelas = kelas + "Komposer";
+            }
+            if (cbKonduktor.Checked)
+            {
+                kelas = kelas + "Konduktor";
+            }
+            if (cbPiano.Checked)
+            {
+                kelas = kelas + "Piano";
+            }
+            if (cbSaxophone.Checked)
+            {
+                kelas = kelas + "Saxophone";
+            }
+            if (cbVokal.Checked)
+            {
+                kelas = kelas + "Vokal";
+            }
+            if (kelas == "")
+            {
+                MessageBox.Show("Harus memilih salah satu dari pilihan kelas", "Warning!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
